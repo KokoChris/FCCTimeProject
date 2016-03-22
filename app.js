293502fbf7e01e4f693f6cc1f891f628eaa2c22b
kokoChris <kokolios1986@yahoo.gr>
2016-03-22 20:08:05 +0200

    minor changes mostly playing around

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ var express = require("express"),
     moment  = require("moment"),
     app     = express();
 
+app.get("/",function (req,res) {
+
+	res.send("This is the root route!");
+});
 
 
-app.get("/", function(req,res){
-	res.json("Hey");
+app.get("/:dateFormat", function(req,res){
+
+	var m = req.params.detFormat;
+	console.log(moment().unix());
+	
+
+	res.redirect("/");
+	//console.log(moment(req.params));
+	//res.json(req.param);
 });
 
 app.listen(3000,function(){
